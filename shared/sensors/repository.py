05c9a0de9b6a53d528e4fa898c89b1a7900a9d1e
Mohsin Zaman <mohsinstyle786@gmail.com>
2024-05-16 01:13:04 +0200

    Update repository.py

--- shared/sensors/repository.py
+++ shared/sensors/repository.py
@@ -164,6 +164,8 @@ def get_data(redis: redis_client.RedisClient, sensor_id: int, db:Session, mongod
         raise HTTPException(status_code=404, detail="Sensor not found")
     sensorDataDB = redis.get( f"sensor:{sensor_id}:data")
     sensor_data = json.loads(sensorDataDB.decode())
+    print("GETTING THIS?", db_sensor.id)
+    print("sensor data?", sensor_data)
     sensor_data['id'] = db_sensor.id
     sensor_data['name'] = db_sensor.name
     return sensor_data
