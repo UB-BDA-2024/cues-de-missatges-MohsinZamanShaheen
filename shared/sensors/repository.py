38147467a401a09c04a8fd973c82beb3bb469701
Mohsin Zaman <mohsinstyle786@gmail.com>
2024-05-16 03:14:55 +0200

    attempt

--- shared/sensors/repository.py
+++ shared/sensors/repository.py
@@ -129,6 +129,11 @@ def record_data(cassandra: CassandraClient,redis: redis_client, timescale: times
     query = f"""
         INSERT INTO sensor_data (sensor_id, velocity, temperature, humidity, battery_level, time)
         VALUES ({sensor_id}, {velocity}, {temperature}, {humidity}, {data.battery_level}, '{data.last_seen}')
+        ON CONFLICT (time,sensor_id) DO UPDATE SET
+        velocity = EXCLUDED.velocity,
+        temperature = EXCLUDED.temperature,
+        humidity = EXCLUDED.humidity,
+        battery_level = EXCLUDED.battery_level;
         """
     timescale.execute(query)
     timescale.conn.commit()
@@ -137,13 +142,13 @@ def record_data(cassandra: CassandraClient,redis: redis_client, timescale: times
     
     logging.debug(f"Recording data for Sensor ID {sensor_id} with data: {data}")
 
-    # # Construct the insert query using safer formatting to prevent SQL injection-like issues
-    # insert_query = f"""
-    # INSERT INTO sensor_data_tbl (sensor_id, timestamp, temperature, humidity, velocity, battery_level)
-    # VALUES ({sensor_id}, {timestamp}, {temperature}, {humidity}, {velocity}, {battery_level});
-    # """
+    # Construct the insert query using safer formatting to prevent SQL injection-like issues
+    insert_query = f"""
+    INSERT INTO sensor_data_tbl (sensor_id, timestamp, temperature, humidity, velocity, battery_level)
+    VALUES ({sensor_id}, {timestamp}, {temperature}, {humidity}, {velocity}, {battery_level});
+    """
     try:
-        #cassandra.execute(insert_query)
+        cassandra.execute(insert_query)
         print("Data inserted successfully into Cassandra.")
         if temperature is not None:
             update_temperature_statistics(cassandra, sensor_id, temperature)
